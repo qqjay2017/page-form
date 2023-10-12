dd9a114ed96215b0935dd4ed456826b23771ead5
huangbo <qqjay2013@gmail.com>
2023-10-12 16:31:45 +0800

    inti project

---
+++ README.md
@@ -0,0 +1,31 @@
+### .env 样例
+
+```
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_ZG9taW5hbnQtY2hhbW9pcy0xLmNsZXJrLmFjY291bnRzLmRldiQ
+CLERK_SECRET_KEY=sk_test_Y9kPE3Oql2IzqrBkECNtHpQHwNMDQOxjEASvapDoXj
+
+
+NEXT_PUBLIC_CLERK_SIGN_IN_URL=/sign-in
+NEXT_PUBLIC_CLERK_SIGN_UP_URL=/sign-up
+NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL=/
+NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL=/
+
+
+
+POSTGRES_PRISMA_URL=
+POSTGRES_URL_NON_POOLING=
+```
+
+### 初始化数据库
+
+```
+npx prisma migrate dev
+init db
+
+```
+
+### 打开可视化数据库
+
+```
+npx prisma studio
+```
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+import Logo from "@/components/Logo";
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+      <nav>
+        <Logo />
+        <ThemeSwitcher />
+        <UserButton afterSignOutUrl="/sign-in" />
+      </nav>
+      <main className="flex w-full flex-grow">{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
